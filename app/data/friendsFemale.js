a6484f2a918af6425b39e9b5efd82ae8a3c87bfe
Tian Qin <qintian29@gmail.com>
2019-12-15 22:55:15 +0800

    f

---
+++ app/data/friendsFemale.js
@@ -0,0 +1,39 @@
+var userList = [
+  {
+    name: "Lucy",
+    gender: "female",
+    photo:
+      "https://secure.i.telegraph.co.uk/multimedia/archive/03249/archetypal-female-_3249633c.jpg",
+    scores: [3, 4, 5, 4, 3, 3, 4, 2, 1, 2]
+  },
+  {
+    name: "Karen",
+    gender: "female",
+    photo:
+      "https://www.esafety.gov.au/sites/default/files/2019-07/image-women-social_media.jpg",
+    scores: [1, 4, 2, 4, 5, 3, 5, 4, 3, 2]
+  },
+  {
+    name: "Mia",
+    gender: "female",
+    photo:
+      "https://cdn.makeupandbeauty.com/wp-content/uploads/2017/03/10-Makeup-and-Hairstyling-Tips-for-Women-with-Thin-Faces2-700x556.jpg",
+    scores: [2, 5, 5, 4, 1, 1, 1, 2, 1, 2]
+  },
+  {
+    name: "Kathy",
+    gender: "female",
+    photo:
+      "https://ugc.reveliststatic.com/gen/constrain/640/640/80/2019/04/19/11/e2/dy/ph9dy4ns0g2qbwe.png",
+    scores: [3, 3, 1, 2, 5, 5, 4, 2, 1, 2]
+  },
+  {
+    name: "Helen",
+    gender: "female",
+    photo:
+      "https://i.pinimg.com/originals/bd/b0/40/bdb0409f08d0ffd8bc732f4110cc6ad9.jpg",
+    scores: [3, 3, 1, 4, 3, 4, 1, 4, 5, 2]
+  }
+];
+
+module.exports = userList;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,45 @@
-var 
+var friendsMale = require("../data/friendsMale");
+var friendsFemale = require("../data/friendsFemale");
+
+function matchingCompare(a, b) {
+  var matchingScore = 0;
+  for (let i = 0; i < a.scores.length; i++) {
+    matchingScore += Math.abs(
+      parseInt(a.scores[i].val()) - parseInt(b.scores[i].val())
+    );
+  }
+  return matchingScore;
+}
+
+module.exports = function(app) {
+  app.get("/api/friendsmale", function(req, res) {
+    res.json("../data/friendsMale.js");
+  });
+  app.get("/api/friendsfemale", function(req, res) {
+    res.json("../data/friendsFemale.js");
+  });
+
+  app.post("/api/friendsmale", function(req, res) {
+    friendsMale.push(req.body);
+    var comparisonArray = [];
+    for (let i = 0; i < friendsFemale.length; i++) {
+      comparisonArray.push(matchingCompare(req.body, friendsFemale[i]));
+    }
+    let bestMatchScore = Math.min(comparisonArray);
+    let bestMatchIndex = comparisonArray.findIndex(s => s == bestMatchScore);
+    res.json(friendsFemale[bestMatchIndex]);
+    comparisonArray = [];
+  });
+
+  app.post("/api/friendsfemale", function(req, res) {
+    friendsFemale.push(req.body);
+    var comparisonArray = [];
+    for (let i = 0; i < friendsMale.length; i++) {
+      comparisonArray.push(matchingCompare(req.body, friendsMale[i]));
+    }
+    let bestMatchScore = Math.min(comparisonArray);
+    let bestMatchIndex = comparisonArray.findIndex(s => s == bestMatchScore);
+    res.json(friendsMale[bestMatchIndex]);
+    comparisonArray = [];
+  });
+};
