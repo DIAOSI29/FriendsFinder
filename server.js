e5b9e74f8d537d7d7fa7d0628c2318dcf4f3ff31
Tian Qin <qintian29@gmail.com>
2019-12-22 01:25:03 +0800

    changes

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var express = require("express");
 var app = express();
+const path = require("path");
 
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + "/public"));
-console.log(__dirname);
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("app/public"));
+
+// app.get("/static/:filename", (req, res) => {
+//   var fileName = req.params.filename;
+//   res.sendFile(path.join(__dirname, "app", "public", fileName));
+// });
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
